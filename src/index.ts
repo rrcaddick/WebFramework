a2d307e5fd322ee75bb000875406356b0c15b062
Ray Caddick <rrcaddick@gmail.com>
2024-03-07 17:39:19 +0200

    Completed initial User model

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,5 @@
 import { User } from "./models/User";
 
-const user = new User({ name: "Ray", age: 34 });
+const user = new User({ id: 1, name: "Raymond", age: 34 });
 
-user.on("change", () => {
-  console.log("1st Change");
-});
-user.on("change", () => {
-  console.log("2st Change");
-});
-user.on("click", () => {
-  console.log("You just clicked");
-});
-
-user.trigger("change");
-user.trigger("click");
-user.trigger("save");
+user.save();

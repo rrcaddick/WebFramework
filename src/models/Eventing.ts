e7e94e6c34ea1d6cf42b006f5783ad9aecd7a208
Ray Caddick <rrcaddick@gmail.com>
2024-03-08 11:00:33 +0200

    Merged changes

--- src/models/Eventing.ts
+++ src/models/Eventing.ts
@@ -1,15 +1,6 @@
 type Callback = () => void;
 
-<<<<<<< HEAD
 export class Eventing {
-=======
-export interface Eventing {
-  on(eventName: string, callback: Callback): void;
-  trigger(eventName: string): void;
-}
-
-export class CustomEventing implements Eventing {
->>>>>>> c55ccea1bc4d98101033ede69cf6cee8067dd8c6
   private events: { [key: string]: Callback[] } = {};
 
   on(eventName: string, callback: Callback): void {
